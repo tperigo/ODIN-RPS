45f89d3ed36a61c58bdbf8a64550b3840dc0e0d0
Theo Perigo <tperigo@akatsuki.lan>
2023-08-08 20:44:18 -0600

    coded game logic js, created html page, added css styling

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
-function getComputerChoice() {
-    let choice = Math.random(3) + 1;
-    console.log(choice);
-}
+const weapons = ["rock", "paper", "scissors"];
+const getComputerChoice = () => weapons[Math.floor(Math.random() * weapons.length)];
 
-getComputerChoice();
+let playerScore = 0;
+let cpuScore = 0;
+
+document.getElementById("rock").addEventListener("click", () => playRPS("rock", getComputerChoice()));
+document.getElementById("paper").addEventListener("click", () => playRPS("paper", getComputerChoice()));
+document.getElementById("scissors").addEventListener("click", () => playRPS("scissors", getComputerChoice()));
+
+function playRPS(playerSelection, computerSelection) {
+    let resultMessage;
+
+    if (playerSelection === computerSelection) {
+        resultMessage = "It's a tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        resultMessage = "You win!";
+        playerScore++;
+    } else {
+        resultMessage = "Computer wins!";
+        cpuScore++;
+    }
+
+    const resultElement = document.getElementById("result");
+    resultElement.textContent = `You chose ${playerSelection}, Computer chose ${computerSelection}. ${resultMessage}`;
+
+    const scoresElement = document.getElementById("scores");
+    scoresElement.textContent = `Player Score: ${playerScore} - CPU Score: ${cpuScore}`;
+    if (playerScore === 5) {
+        scoresElement.textContent = `Player wins! Game Over`;
+        playerScore = 0;
+        cpuScore = 0;
+    }
+    if (cpuScore === 5) {
+        scoresElement.textContent = `CPU wins! Game Over`;
+        playerScore = 0;
+        cpuScore = 0;
+    }
+
+    console.log(resultMessage);
+}
